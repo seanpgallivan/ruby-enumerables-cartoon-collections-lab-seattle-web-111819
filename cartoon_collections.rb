fdfd774fb81bae4b3dd47d784a1e3a7f342f3401
Sean Gallivan <seanpgallivan@hotmail.com>
2019-11-14 22:52:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,8 +15,7 @@ def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |item|
-    i = 0
-    while i < cheese_types.length do
+    item = cheese_types[0]
       
     
 end
