af5c89eed508fb05d7b38a5129d3444fc1ffce05
Sean Gallivan <seanpgallivan@hotmail.com>
2019-11-14 22:36:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,9 +6,9 @@ def summon_captain_planet(array)
   array.map{|call| "#{call.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+def long_planeteer_calls(array)
+  
+  end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
